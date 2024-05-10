7fc5c40c033195c4c750d33e6a86d8c60f662e31
Lapyae Pyho <greenarchermoon@gmail.com>
2024-05-11 00:13:12 +0900

    finish flask practice project

---
+++ Developing_AI_applications_with_python_and_flask/best_practice/sample2.py
@@ -0,0 +1,10 @@
+"""Module provide addition function"""
+
+def add(number1, number2):
+    """ return the result of addition number1 to number2"""
+    return number1 + number2
+
+NUM1 = 4
+NUM2 = 5
+TOTAL = add(NUM1, NUM2)
+print(f"The sum of {NUM1} and {NUM2} is {TOTAL}")
---
+++ Developing_AI_applications_with_python_and_flask/mymath/geometry.py
@@ -0,0 +1,11 @@
+def area_of_rectangle(lenght, width):
+    """
+    This function returns the area of rectangle of given length and width
+    """
+    return lenght * width
+
+def area_of_circle(radius):
+    """
+    This function returns the area of circle of given radius
+    """
+    return 3.142 * (radius ** 2)
