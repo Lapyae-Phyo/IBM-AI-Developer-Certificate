897762acf8c86a1cc0f84ba83ba714ed7c785879
Lapyae Pyho <greenarchermoon@gmail.com>
2024-05-11 16:47:08 +0900

    finish simple calculator flask app

---
+++ Developing_AI_applications_with_python_and_flask/simple_calculator_flask_app/Maths/mathematics.py
@@ -0,0 +1,12 @@
+def summation(a, b):
+    try:
+        result =  a + b
+    except:
+        return "error"
+    return result
+
+def subtraction(a, b):
+    return a - b
+
+def multipication(a, b):
+    return a * b
---
+++ Developing_AI_applications_with_python_and_flask/simple_calculator_flask_app/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from Maths.mathematics import summation, subtraction, multipication
+
+app = Flask("Mathematics Problem Solver")
+
+@app.route("/sum")
+def sum_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    result = summation(num1, num2)
+    return result
+
+@app.route("/sub")
+def sub_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    result = subtraction(num1, num2)
+    return result
+
+@app.route("/mul")
+def mul_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    result = multipication(num1, num2)
+    return result
+
+@app.route("/")
+def render_index_page():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
