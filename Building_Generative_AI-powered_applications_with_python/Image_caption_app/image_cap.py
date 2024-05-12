4e4ee8d05ac4af2ef94cf89b49c38804529e6f50
Lapyae Pyho <greenarchermoon@gmail.com>
2024-05-12 22:51:14 +0900

    finish image_captioning_app

---
+++ Building_Generative_AI-powered_applications_with_python/Image_caption_app/image_cap.py
@@ -0,0 +1,25 @@
+from PIL import Image
+from transformers import AutoProcessor, BlipForConditionalGeneration
+
+# Load the pretrained processor and model
+processor = AutoProcessor.from_pretrained(
+    "Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained(
+    "Salesforce/blip-image-captioning-base")
+
+# Load your image
+img_path = "/Users/lapyaephyo/Pictures/3412.jpg"
+# convert it into an RGB format
+image = Image.open(img_path).convert("RGB")
+
+# You do not need a question for image captioning
+text = "the image of"
+inputs = processor(images=image, text=text, return_tensors="pt")
+
+# Generate a caption for the image
+outputs = model.generate(**inputs, max_length=50)
+
+# Decode the generated tokens to text
+caption = processor.decode(outputs[0], skip_special_tokens=True)
+# Print the caption
+print(caption)
